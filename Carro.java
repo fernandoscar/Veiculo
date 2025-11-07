174556738bffe938893fb34e1d21db1c400feefb
Fernando Henrique Silva de Carvalho <fernando.scarvalho@sempreceub.com>
2025-11-07 09:41:15 -0300

    Refactor Carro.java by removing comments
    Removed unnecessary comments and cleaned up code.

--- Carro.java
+++ Carro.java
@@ -1,21 +1,10 @@
-/**
- * Classe Concreta para "Carro".
- * Conforme Item 2 do PDF.
- *
- * 1. Herda de Veiculo (extends Veiculo)
- * 2. Implementa a interface Locavel (implements Locavel)
- */
+
 public class Carro extends Veiculo implements Locavel {
 
-    /**
-     * Atributo próprio da classe Carro (Item 2.24)
-     */
+
     private int numeroDePortas;
 
-    /**
-     * Construtor da classe Carro.
-     * Ele recebe os dados do Carro e também os dados que o "Veiculo" (pai) precisa.
-     */
+
     public Carro(String placa, String marca, String modelo, double valorDiaria, int numeroDePortas) {
         // "super(...)" chama o construtor da classe PAI (Veiculo)
         // para inicializar os atributos que são herdados.
@@ -27,11 +16,7 @@ public class Carro extends Veiculo implements Locavel {
 
     // --- 1. Implementação do Método Abstrato (de Veiculo) ---
 
-    /**
-     * Implementação específica para Carro.
-     * Regra: Aplica 10% de desconto sobre o valor da diária.
-     * (Item 2.25)
-     */
+
     @Override
     public double calcularDiariaComDesconto() {
         // getValorDiaria() é um método herdado de Veiculo
@@ -43,10 +28,7 @@ public class Carro extends Veiculo implements Locavel {
 
     // --- 2. Implementação dos Métodos da Interface (de Locavel) ---
 
-    /**
-     * Implementa o contrato 'iniciarLocacao' da interface Locavel.
-     * Altera o status para LOCADO.
-     */
+
     @Override
     public void iniciarLocacao() {
         // setStatus() é um método herdado de Veiculo
@@ -54,20 +36,14 @@ public class Carro extends Veiculo implements Locavel {
         System.out.println("Carro " + this.getModelo() + " foi locado com sucesso.");
     }
 
-    /**
-     * Implementa o contrato 'encerrarLocacao' da interface Locavel.
-     * Altera o status para DISPONIVEL.
-     */
+
     @Override
     public void encerrarLocacao() {
         this.setStatus(StatusVeiculo.DISPONIVEL);
         System.out.println("Carro " + this.getModelo() + " foi devolvido e está disponível.");
     }
 
-    /**
-     * Implementa o contrato 'verificarDisponibilidade' da interface Locavel.
-     * Retorna true se o status for DISPONIVEL.
-     */
+
     @Override
     public boolean verificarDisponibilidade() {
         // getStatus() é um método herdado de Veiculo
